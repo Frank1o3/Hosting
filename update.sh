e49a1b4e0ee0b874cec1c3d5096eb32d88b0ca9c
Frank1o3 <jahdy1o3@gmail.com>
2024-04-29 22:43:12 +0000

    deleted:    dist/Update.js 	deleted:    src/Update.ts 	renamed:    test.sh -> update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+source .env
+
 # Fetch the current public IP address
 CURRENT_IP=$(curl -s https://api.ipify.org)
+
+LOCAL_IP=$(hostname -I | awk '{print $1}')
+
+URL=$"http://$LOCAL_IP:$PORT"
 
 # Define the path to your Nginx configuration file
 NGINX_CONFIG_FILE="/etc/nginx/sites-available/my-node-app"
 
 # Use sed to replace the IP address in the configuration file
-sudo sed -i "s/server_name .*/server_name $CURRENT_IP;/" $NGINX_CONFIG_FILE
+
+echo "LOCAL IP: $LOCAL_IP PORT: $PORT PUBLIC IP: $CURRENT_IP URL: $URL"
+
+sudo sed -i "s|server_name .*|server_name $CURRENT_IP;|" $NGINX_CONFIG_FILE
+
+sudo sed -i "s|proxy_pass .*|proxy_pass $URL;|" $NGINX_CONFIG_FILE
 
 # Test the Nginx configuration for syntax errors
 sudo nginx -t
